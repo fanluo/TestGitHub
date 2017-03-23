bb03f2ec85178931a06baf74b8339bc6562e37fe
luofan <luofan@g7.com.cn>
2017-03-23 17:42:29 +0800

    test update

--- MyApplication/app/src/main/java/andy/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/andy/myapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package andy.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.btn);
+        mButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 测试内容
+            }
+        });
     }
 }
